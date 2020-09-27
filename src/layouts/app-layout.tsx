c3286baee2999e349ab2c531ece421515ed19d9f
EC2 Default User <ec2-user@ip-172-31-40-160.eu-central-1.compute.internal>
2020-09-27 05:19:21 +0000

    init for vercel deployment

---
+++ src/layouts/app-layout.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import Sticky from 'react-stickynode';
+import { useAppState } from 'contexts/app/app.provider';
+import Header from './header/header';
+import { LayoutWrapper } from './layout.style';
+import { isCategoryPage } from './is-home-page';
+import useStores from "hooks/useStores";
+import Grid from '@material-ui/core/Grid';
+import Sidebar from 'react-sidebar';
+const MobileHeader = dynamic(() => import('./header/mobile-header'), {
+  ssr: false,
+});
+
+const SidebarMenu = dynamic(() => import('src/layouts/sidebar/sidebarMenu'));
+import { push as Menu } from 'react-burger-menu'
+// import set = Reflect.set;
+// type LayoutProps = {
+//   className?: string;
+//   token?: string;
+//   children?: any;
+//
+// };
+
+const Layout = ({
+  className,
+  children,
+  token,
+deviceType
+}) => {
+  // console.log(JSON.stringify(deviceType))
+
+  const {uiStore} = useStores();
+  // const [isMenu, setMenu] = React.useState(true)
+
+  // React.useEffect(() => {
+  //   setMenu(uiStore.isMenuDrawerOpen)
+  // }, [uiStore.isMenuDrawerOpen]);
+
+  console.log(uiStore)
+  const isSticky = useAppState('isSticky');
+  const { pathname, query } = useRouter();
+  const type = pathname === '/restaurant' ? 'restaurant' : query.type;
+
+  const isHomePage = isCategoryPage(type);
+  return (
+      <LayoutWrapper >
+
+        <Sticky enabled={isSticky} innerZ={999}>
+          <MobileHeader
+            className={`${isSticky ? 'sticky' : 'unSticky'} ${
+              isHomePage ? 'home' : ''
+            } desktop`}
+          />
+          <Header
+            className={`${isSticky && isHomePage ? 'sticky' : 'unSticky'} ${
+              isHomePage ? 'home' : ''
+            }`}
+          />
+        </Sticky>
+        <div id="rrr-outer-container">
+
+        {/*<Grid container>*/}
+        {/*  <Grid  item xs={2} style={isMenu ? {} : { display: 'none' }}>*/}
+        {/*    <div style={{background: "#fff", height: "100%"}}>*/}
+        {/*      <SidebarMenu type={'grocery'} deviceType={{desktop: true, mobile: 'false', tablet: 'false'}}  />*/}
+        {/*    </div>*/}
+        {/*  </Grid>*/}
+        {/*  <Grid  item xs={isMenu ? 10:12} >*/}
+        {/*    {children}*/}
+        {/*  </Grid>*/}
+        {/*</Grid>*/}
+
+          <Sidebar
+            sidebar={ <div id={"rrr-sidebar"}><SidebarMenu type={'grocery'} deviceType={deviceType}  /></div>}
+            open={uiStore.isMenuDrawerOpen}
+            styles={{ sidebar: { background: "white" } }}
+            shadow={!!deviceType.mobile}
+            docked={!deviceType.mobile && uiStore.isMenuDrawerOpen}
+            transitions={true}
+            sidebarId={"rrr-sidebar"}
+            contentId={"rrr-content"}
+          >
+            <div id={"rrr-content"}>
+                {children}
+            </div>
+          </Sidebar>
+
+
+
+        {/*<Menu noOverlay isOpen={isMenu} onOpen={setMenu} pageWrapId={ "rrr-page-wrap" } outerContainerId={ "rrr-outer-container" }>*/}
+        {/*    <SidebarMenu type={'grocery'} deviceType={deviceType}  />*/}
+        {/*  </Menu>*/}
+
+
+
+
+        </div>
+
+
+      </LayoutWrapper>
+  );
+};
+
+export default Layout;
