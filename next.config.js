6cecf035fbbc6c59686d37f50a60db6c7e0e8ab6
Ubuntu <ubuntu@ip-172-31-11-36.eu-central-1.compute.internal>
2020-11-20 16:01:48 +0000

    s

--- next.config.js
+++ next.config.js
@@ -59,7 +59,16 @@ const nextConfig = {
       'img5.etsystatic.com',
       'img6.etsystatic.com',
       'www.artwaley.com',
-      'lh3.googleusercontent.com'
+      'lh3.googleusercontent.com',
+      'lh4.googleusercontent.com',
+      'lh1.googleusercontent.com',
+      'lh2.googleusercontent.com',
+      'lh5.googleusercontent.com',
+      'lh6.googleusercontent.com',
+      'lh7.googleusercontent.com',
+      'lh8.googleusercontent.com',
+      'lh9.googleusercontent.com',
+      'lh0.googleusercontent.com'
     ]
   },
   webpack(webpackConfig) {
